ec96948813062d06ab9583d15485fec82565db77
Sakarya <sakaryaa@685b35a94606.ant.amazon.com>
2015-12-27 00:44:09 -0500

    Initiated EBS Import function process

--- application.py
+++ application.py
@@ -56,6 +56,53 @@ def s3im():
         typeOfExport = form.typeOfExport.data
         return render_template('s3imresults.html', form=form)
 
+@application.route('/ebsim', methods=['GET', 'POST'])
+def ebsim():
+    form = S3ImportForm()
+    if request.method == 'GET':
+        return render_template('ebsim.html', form=form)
+    elif request.method == 'POST':
+        deviceId = form.deviceId.data
+        notificationEmail = form.notificationEmail.data
+        acl = form.acl.data
+        bucket = form.bucket.data
+        logPrefix = form.logPrefix.data
+        prefix = form.prefix.data
+        logBucket = form.logBucket.data
+        trueCryptPassword = form.trueCryptPassword.data
+        pinCode = form.pinCode.data
+        cacheControl = form.cacheControl.data
+        contentDisposition = form.contentDisposition.data
+        contentLanguage = form.contentLanguage.data
+        contentTypes = form.contentTypes.data
+        diskTimestampMetadataKey = form.diskTimestampMetadataKey.data
+        expires = form.expires.data
+        ignore = form.ignore.data
+        setContentEncodingForGzFiles = form.setContentEncodingForGzFiles.data
+        staticMetadata = form.staticMetadata.data
+        storageClass = form.storageClass.data
+        substitutions = form.substitutions.data
+        serviceLevel = form.serviceLevel.data
+        name = form.name.data
+        company = form.company.data
+        street1 = form.street1.data
+        street2 = form.street2.data
+        street3 = form.street3.data
+        city = form.city.data
+        stateOrProvince = form.stateOrProvince.data
+        postalCode = form.postalCode.data
+        phoneNumber = form.phoneNumber.data
+        country = form.country.data
+        dataDescription = form.dataDescription.data
+        encryptedData = form.encryptedData.data
+        exportCertifierName = form.exportCertifierName.data
+        requiresExportLicense = form.requiresExportLicense.data
+        deviceValue = form.deviceValue.data
+        deviceCountryOfOrigin = form.deviceCountryOfOrigin.data
+        deviceType = form.deviceType.data
+        typeOfExport = form.typeOfExport.data
+        return render_template('ebsimresults.html', form=form)
+
 
 # run the app.
 if __name__ == "__main__":
